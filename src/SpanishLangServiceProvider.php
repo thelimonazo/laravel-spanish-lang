70d0ab2d7e295a80f86f5eaddb4661cfaca1766c
Isaac Limón <limonazzzo@gmail.com>
2025-04-24 22:50:53 -0600

    Spanish language files for Laravel

---
+++ src/SpanishLangServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TheLimonazo\SpanishLang;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application;
+
+class SpanishLangServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // Determine the base path according to the Laravel version
+        $basePath = version_compare(Application::VERSION, '9.0', '<')
+            ? resource_path('lang/es')
+            : base_path('lang/es');
+
+        // Publish the language files
+        $this->publishes([
+            __DIR__.'/../lang' => $basePath,
+        ], 'laravel-lang-spanish');
+    }
+}
